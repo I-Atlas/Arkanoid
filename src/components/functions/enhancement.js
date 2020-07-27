a97002c04707d688c17e1e40fba993bf0d7e8f76
I-Atlas <54947380+I-Atlas@users.noreply.github.com>
2020-07-27 14:54:57 +0300

    Update №9

---
+++ src/components/functions/enhancement.js
@@ -0,0 +1,16 @@
+import { game } from "../constants"
+import { height } from "./functions"
+
+/* ENHANCEMENT CLASS
+The class responsible for the enhancements.
+*/
+export default class Enhancement {
+    constructor(x, y, size, type) {
+        this.width = size
+        this.height = size
+        this.x = x
+        this.y = y
+        this.type = type
+        this.speedY = game.ENHANCEMENT_SPEED * height
+    }
+}
